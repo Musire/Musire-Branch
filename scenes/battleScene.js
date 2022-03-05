99b8874a419bb59f3b947be823272ed6e5c1dc44
Musire <63882131+Musire@users.noreply.github.com>
2022-03-04 17:57:08 -0700

    updated the correct tweens for the sprites

--- scenes/battleScene.js
+++ scenes/battleScene.js
@@ -6,13 +6,31 @@ class BattleScene extends Phaser.Scene {
 
     preload() {
         this.load.image('blankScene', 'assets/sprites/blankScene.png');
+        this.load.image('opponent', 'assets/sprites/blue.png');
+        this.load.image('player', 'assets/sprites/red2.png');
+
     }
 
     create() {
-            var bg = this.add.sprite(0,0,'blankScene');
-            bg.setOrigin(0,0);
-
-            var text = this.add.text(100,100, 'Welcome to my game!');
+            // create the canvas of the scene
+            this.blankScene = this.add.sprite(400, 400,'blankScene');
+            this.opponent = this.add.sprite(350, 360, 'opponent')
+            this.player = this.add.sprite(445, 395, 'player')
+            this.banner = this.add.text(300,100, 'Welcome to my game!');    
+            
+            this.tweens.add({
+                targets: this.opponent,
+                duration: 1000,
+                x: 450,
+                repeat: 0,
+            })
+            
+            this.tweens.add({
+                targets: this.player,
+                duration: 1000,
+                x: 355,
+                repeat: 0,
+            })
     }
 
 }
