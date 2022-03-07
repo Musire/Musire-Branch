8e36477274199f21a509953e0abe1207bbefca18
Musire <63882131+Musire@users.noreply.github.com>
2022-03-06 17:06:40 -0700

    added comments to the battleScene file

--- scenes/battleScene.js
+++ scenes/battleScene.js
@@ -5,6 +5,8 @@ class BattleScene extends Phaser.Scene {
     }
 
     preload() {
+
+        //preload blankscene, opponent, player, and song
         this.load.image('blankScene', 'assets/sprites/blankScene.png');
         this.load.image('opponent', 'assets/sprites/blue.png');
         this.load.image('player', 'assets/sprites/red2.png');
@@ -15,10 +17,14 @@ class BattleScene extends Phaser.Scene {
     create() {
             // create the canvas of the scene
             this.blankScene = this.add.sprite(400, 400,'blankScene');
+            // create the opponent sprite
             this.opponent = this.add.sprite(350, 360, 'opponent')
+            // create the player sprite
             this.player = this.add.sprite(445, 395, 'player')
+            // test text for the game
             this.banner = this.add.text(300,100, 'Welcome to my game!');    
             
+            // tween effect to slide opponent sprite from left to right
             this.tweens.add({
                 targets: this.opponent,
                 duration: 1000,
@@ -26,6 +32,7 @@ class BattleScene extends Phaser.Scene {
                 repeat: 0,
             })
             
+            // tween effect to slide player sprite from right to left
             this.tweens.add({
                 targets: this.player,
                 duration: 1000,
@@ -33,6 +40,7 @@ class BattleScene extends Phaser.Scene {
                 repeat: 0,
             })
 
+            // configuration for the audio file for music
             let musicConfig = {
                 mute: false,
                 volume: 1,
@@ -42,6 +50,8 @@ class BattleScene extends Phaser.Scene {
                 loop: false,
                 delay: 0
             }
+
+            // instantiate the sound component from the preloaded 'song'
             this.music = this.sound.add('song');
             this.music.play(musicConfig);
     }
